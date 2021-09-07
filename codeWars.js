4e8ff176654bc730544f52d00df7d8d80a934ae3
salrey <tekdehn@gmail.com>
2021-09-07 14:37:01 -0400

    Add solution to find magic index kata using .filter

--- codeWars.js
+++ codeWars.js
@@ -3,11 +3,19 @@
 
 //--------------NEW KATA------------------
 
-function numObj(s){
-  return s.map((element) => element = {[element]: String.fromCodePoint(element)})
+function findMagic (arr) {
+  return Number(arr.filter((element,index) => element === index)) || -1
 }
 
-console.log(numObj([118,117,120]))
+console.log(findMagic([-20,-10,2,10,20]))
+
+//--------------NEW KATA------------------
+
+// function numObj(s){
+//   return s.map((element) => element = {[element]: String.fromCodePoint(element)})
+// }
+
+// console.log(numObj([118,117,120]))
 
 //--------------NEW KATA------------------
 
